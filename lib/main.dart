cf166aa3dcc2c7864f272b6870e04c55039d37fb
Theodor Stoica <62388619+Thrauk@users.noreply.github.com>
2023-05-28 18:47:26 +0300

    Quiz match (#7)
    * Added challenge and accept challenge functionality for a test user
    
    * Centered the question screen to look better
    
    * added back emulator functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:trivia_app/src/features/authentication/presentation/blocs/auth_bloc/auth_bloc.dart';
@@ -9,15 +12,15 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  // if (kDebugMode) {
-  //   try {
-  //     FirebaseFirestore.instance.useFirestoreEmulator('10.0.2.2', 8080);
-  //     await FirebaseAuth.instance.useAuthEmulator('10.0.2.2', 9099);
-  //   } catch (e) {
-  //     // ignore: avoid_print
-  //     print(e);
-  //   }
-  // }
+  if (kDebugMode) {
+    try {
+      FirebaseFirestore.instance.useFirestoreEmulator('10.0.2.2', 8080);
+      await FirebaseAuth.instance.useAuthEmulator('10.0.2.2', 9099);
+    } catch (e) {
+      // ignore: avoid_print
+      print(e);
+    }
+  }
 
   runApp(
     MultiBlocProvider(

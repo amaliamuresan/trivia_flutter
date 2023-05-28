1710c4888a5a4106598dd15816e8da918645321f
Amalia Muresan <amaliamuresan1290@gmail.com>
2023-05-28 18:19:53 +0300

    finished manage friend requests

--- lib/src/features/notifications/presentation/widgets/friend_requests_list_widget.dart
+++ lib/src/features/notifications/presentation/widgets/friend_requests_list_widget.dart
@@ -4,8 +4,6 @@ import 'package:trivia_app/src/enums/page_state_enum.dart';
 import 'package:trivia_app/src/features/authentication/presentation/blocs/auth_bloc/auth_bloc.dart';
 import 'package:trivia_app/src/features/notifications/presentation/blocs/manage_friend_requests_bloc/manage_friend_requests_bloc.dart';
 import 'package:trivia_app/src/features/notifications/presentation/widgets/friend_request_item_widget.dart';
-import 'package:trivia_app/src/features/profile/domain/firestore_user_public_data.dart';
-import 'package:trivia_app/src/features/profile/domain/user_data_service.dart';
 
 class FriendRequestsListWidget extends StatelessWidget {
   const FriendRequestsListWidget({super.key});
@@ -13,12 +11,14 @@ class FriendRequestsListWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => ManageFriendRequestsBloc(context.read<AuthBloc>().state.authUserData.id),
+      create: (context) => ManageFriendRequestsBloc(
+        context.read<AuthBloc>().state.authUserData.id,
+      ),
       child: BlocBuilder<ManageFriendRequestsBloc, ManageFriendRequestsState>(
         builder: (context, state) {
           if (state.pageStateEnum == PageStateEnum.loaded) {
             return state.friendRequests.isEmpty
-                ? const Text('No friend requests yet')
+                ? const Center(child: Text('No friend requests yet!'))
                 : Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
@@ -28,10 +28,20 @@ class FriendRequestsListWidget extends StatelessWidget {
                           itemBuilder: (BuildContext context, int index) {
                             return FriendRequestItemWidget(
                               onTapAccept: () =>
-                                  context.read<ManageFriendRequestsBloc>().add(AcceptPressed(state.friendRequests[index].id)),
-                              displayName: state.friendRequests[index].displayName ?? '',
+                                  context.read<ManageFriendRequestsBloc>().add(
+                                        AcceptPressed(
+                                          state.friendRequests[index].id,
+                                        ),
+                                      ),
+                              displayName:
+                                  state.friendRequests[index].displayName ?? '',
                               photoUrl: state.friendRequests[index].photoUrl,
-                              onTapDeny: () => context.read<ManageFriendRequestsBloc>().add(DenyPressed(state.friendRequests[index].id)),
+                              onTapDeny: () =>
+                                  context.read<ManageFriendRequestsBloc>().add(
+                                        DenyPressed(
+                                          state.friendRequests[index].id,
+                                        ),
+                                      ),
                               onTapUser: () {},
                             );
                           },
@@ -44,7 +54,7 @@ class FriendRequestsListWidget extends StatelessWidget {
                     ],
                   );
           } else if (state.pageStateEnum == PageStateEnum.error) {
-            return Text('Something went wrong');
+            return const Center(child: Text('Something went wrong'));
           } else {
             return const Center(child: CircularProgressIndicator());
           }
